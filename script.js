8bfb236fbe78f13e61d1bbfa28c7aae3ddf45765
Cyberwitch <73303101+Cyberwitch13@users.noreply.github.com>
2022-05-05 19:25:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function validateForm() {
+let x = document.forms["cats"]["email"].value;
+    if (x =="") {
+  alert("Email es obligatorio");}
+  else  {alert("Enviado")};}
+
+function gracias() {alert("Gracias por donar!");
+console.log("Donacion");}
+
